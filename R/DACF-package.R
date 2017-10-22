b561405734fe3ec33065ac921553213ce6f74405
Qimin Liu <qliu6@nd.edu>
2017-10-22 19:19:38 -0400

    fixed minor error

--- R/DACF-package.R
+++ R/DACF-package.R
@@ -3,12 +3,12 @@
 #' The \code{DACF} package is used to conduct data analyses for data with
 #' ceiling/floor effects. The current version contains functions to
 #' estimate mean and variances and to conduct mean comparison tests.
-
 #'
 #' @section Usage:
 #' To access this package's tutorial, type the following line into the console:
 #'
 #' \code{vignette("DACF-vignette")}
+#'
 #' @docType package
 #' @name DACF-package
 #' @aliases DACF
