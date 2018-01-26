99bc61eae5c73bd77990051c36af3c3eb1cc0cb5
Qimin Liu <qliu6@nd.edu>
2018-01-26 13:47:04 -0500

    updating vignette per Peggy's comments

--- vignettes/DACF-vignette.R
+++ vignettes/DACF-vignette.R
@@ -1,3 +1,41 @@
+## ----echo=FALSE----------------------------------------------------------
+r.1=rnorm(1000,20,5)
+r.2=rnorm(1000,30,5)
+r.3=rnorm(1000,10,10)
+ca.true=matrix(c(r.1,r.2,r.3,rep(c(2,1,3),each=1000)),ncol=2)
+r.2.cf=induce.cfe(0,.3,r.2)
+r.3.cf=induce.cfe(.3,0,r.3)
+ca.cf=matrix(c(r.1,r.2.cf,r.3.cf,rep(c(2,1,3),each=1000)),ncol=2)
+colnames(ca.cf)=c('score','group')
+
+## ------------------------------------------------------------------------
+# mean
+aggregate(ca.true[,1],mean,by=list(ca.true[,2]))
+# variance
+aggregate(ca.true[,1],var,by=list(ca.true[,2]))
+
+## ------------------------------------------------------------------------
+# mean
+aggregate(ca.cf[,1],mean,by=list(ca.cf[,2]))
+# variance
+aggregate(ca.cf[,1],var,by=list(ca.cf[,2]))
+
+## ------------------------------------------------------------------------
+# younger-aged group
+young=ca.cf[ca.cf[,2]==1,1]
+rec.mean.var(young) # true mean and variance are 30 and 25
+# older-aged group
+old=ca.cf[ca.cf[,2]==3,1]
+rec.mean.var(old) # true mean and variance are 10 and 100
+
+## ------------------------------------------------------------------------
+# ANOVA
+lw.f.star(data.frame(ca.cf),score~group,"a")
+lw.f.star(data.frame(ca.cf),score~group,"b")
+# t-test
+lw.t.test(young,old,"a")
+lw.t.test(young,old,"b")
+
 ## ------------------------------------------------------------------------
 library(DACF)
 # Simulate healthy data for two groups
